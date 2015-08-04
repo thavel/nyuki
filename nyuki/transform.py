0110256f0d16b08995047195a7038b1248994070
Ludovic Havel <ludovic.havel@surycat.com>
2015-08-04 19:48:39 +0200

    added the 'Converter' class intended to be the base data processing component of a nyuki (SC-2145)

--- nyuki/transform.py
+++ nyuki/transform.py
@@ -17,11 +17,111 @@ There can be only 1 type of rule within a data processing block
 """
 
 
+class _ListRules(list):
+    """
+    Subclass of `list()` that makes sure each element of it is an instance of
+    a predefined class (e.g. `Search()`).
+    """
+    def __init__(self, rule_cls, *iterable):
+        super(_ListRules, self).__init__()
+        self._rule_cls = rule_cls
+        if iterable:
+            self.extend(*iterable)
+
+    def _check_type(self, item):
+        if self._rule_cls is not None and not isinstance(item, self._rule_cls):
+            err = 'item is not of type {cls}'
+            raise TypeError(err.format(cls=self._rule_cls.__name__))
+
+    def append(self, item):
+        """
+        Check the object type of 'item' before appending to the list.
+        """
+        self._check_type(item)
+        super(_ListRules, self).append(item)
+
+    def extend(self, iterable):
+        """
+        Scan through 'iterable' to check object types before extending the
+        list.
+        """
+        for item in iterable:
+            self._check_type(item)
+        super(_ListRules, self).extend(iterable)
+
+    def insert(self, index, item):
+        """
+        Check the object type of 'item' before appending to the list.
+        """
+        self._check_type(item)
+        super(_ListRules, self).insert(index, item)
+
+
+class Converter(object):
+    """
+    A base class that can apply a (ordered) list of rules to a dict.
+    """
+    _map_rules_cls = {}
+
+    def __init__(self, rule_cls, rules=[]):
+        if not issubclass(rule_cls, _Rule):
+            err = '{obj} is not a subclass of _Rule'
+            raise TypeError(err.format(obj=rule_cls))
+        self._rule_cls = rule_cls
+        self._rules = _ListRules(rule_cls, rules)
+
+    @property
+    def type(self):
+        return self._rule_cls.__name__
+
+    @property
+    def rules(self):
+        return self._rules
+
+    @classmethod
+    def from_dict(cls, config):
+        """
+        Create a `Converter()` object from dict 'config'. The dict must look
+        like the following:
+            {
+                "rule_cls": <rule-type-name>,   # lower-case
+                "rules": [
+                    {"fieldname": <name>, ...}, # + rule-type dependent items
+                    {"fieldname": <name>, ...},
+                ]
+            }
+        """
+        rule_cls = cls._map_rules_cls[config['rule_cls']]
+        rules = []
+        for params in config['rules']:
+            rules.append(rule_cls(**params))
+        return cls(rule_cls, rules=rules)
+
+    def apply(self, data):
+        """
+        Apply each rule sequentially
+        """
+        for rule in self.rules:
+            rule.apply(data)
+
+
+def register(cls):
+    """
+    A decorator that populates the `Converter` class so as to link rule classes
+    to rule type names.
+    """
+    cls.typename = cls.__name__.lower()
+    Converter._map_rules_cls[cls.typename] = cls
+    return cls
+
+
 class _Rule(object):
     """
     A rule is applied to a dict and may update that dict (in-place update).
     Only one field from the input dict can be processed within a rule.
     """
+    typename = None
+
     def __init__(self, fieldname, **kwargs):
         self.fieldname = fieldname
         self.configure(**kwargs)
@@ -79,6 +179,8 @@ class _MatchSearchRule(_RegexpRule):
     Match and search operations basically share a common logic: when the
     operation produces a match what matters is the resulting groupdict.
     """
+    _regexp_op = None
+
     def _set_params(self, pos=None, endpos=None):
         self.pos = pos
         self.endpos = endpos
@@ -93,7 +195,8 @@ class _MatchSearchRule(_RegexpRule):
             return {}
 
 
-class Search(_RegexpRule):
+@register
+class Search(_MatchSearchRule):
     """
     Scan through a string looking for the first location where the regular
     expression pattern produces a match.
@@ -101,7 +204,8 @@ class Search(_RegexpRule):
     _regexp_op = 'search'
 
 
-class Match(_RegexpRule):
+@register
+class Match(_MatchSearchRule):
     """
     Check if zero or more characters at the beginning of a string match the
     regular expression pattern.
@@ -109,6 +213,7 @@ class Match(_RegexpRule):
     _regexp_op = 'match'
 
 
+@register
 class Sub(_RegexpRule):
     """
     Build a string obtained by replacing the leftmost non-overlapping
@@ -124,6 +229,7 @@ class Sub(_RegexpRule):
         return {self.fieldname: res}
 
 
+@register
 class Set(_Rule):
     """
     Set or update a field in the `data` dict.
@@ -135,6 +241,7 @@ class Set(_Rule):
         data[self.fieldname] = self.value
 
 
+@register
 class Unset(_Rule):
     """
     Remove a field from the `data` dict.
@@ -149,6 +256,7 @@ class Unset(_Rule):
             pass
 
 
+@register
 class Lookup(_Rule):
     """
     Implements a lookup table where the input value from `data` must match a
@@ -156,7 +264,8 @@ class Lookup(_Rule):
     """
     def configure(self, table={}):
         """
-        'table' is a dict where keys can be a dict or a string (see LookupTable)
+        'table' is a dict where keys can be a dict or a string that represents
+        the regexp pattern (see LookupTable).
         """
         self.table = LookupTable(table)
 
@@ -177,10 +286,11 @@ class Lookup(_Rule):
 # http://stackoverflow.com/questions/2060972/subclassing-python-dictionary-to-override-setitem
 class LookupTable(dict):
     """
-    Simple wrapper around dict() to build a lookup table where keys are compiled
-    regular expressions.
+    Simple wrapper around dict() to build a lookup table where keys are
+    compiled regular expressions.
     """
     def __init__(self, *args, **kwargs):
+        super(LookupTable, self).__init__()
         self.update(*args, **kwargs)
 
     def __setitem__(self, re_spec, value):
@@ -210,21 +320,3 @@ class LookupTable(dict):
         if key not in self:
             self[key] = value
         return self[key]
-
-
-class _Converter(object):
-    """
-    A base class that can execute an ordered list of operations to a dict and
-    outputs an updated dict.
-    """
-    def __init__(self):
-        self._ops = []
-
-    def append(self):
-        raise NotImplementedError
-
-    def insert(self):
-        pass
-
-    def apply(self):
-        raise NotImplementedError
