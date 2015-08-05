7121f8c1ec2831b00abc0d71ebbd135adedfb572
Guillaume Desvé <guillaume.desve@surycat.com>
2015-08-05 17:17:40 +0200

    Ground work for a new Nyuki python library with circle.ci configuration

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+
+with open('VERSION.txt') as fp:
+    version = fp.read().strip()
+
+# Requirements
+install_reqs = parse_requirements('requirements.txt', session='dummy')
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='nyuki',
+    version=version,
+    description='Nyuki library',
+    author='Optiflows R&D',
+    author_email='rand@surycat.com',
+    install_requires=reqs,
+    packages=find_packages(exclude=['tests'])
+)
