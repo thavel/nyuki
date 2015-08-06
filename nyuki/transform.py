0638381c69b3b651daa1f6a459e523991ff68732
Ludovic Havel <ludovic.havel@surycat.com>
2015-08-06 09:37:43 +0200

    implemented Python registry pattern to register rule classes

--- nyuki/transform.py
+++ nyuki/transform.py
@@ -17,6 +17,27 @@ There can be only 1 type of rule within a data processing block
 """
 
 
+# Inspired from https://github.com/faif/python-patterns/blob/master/registry.py
+class _RegisteredRules(type):
+    """
+    This metaclass is designed to register automatically public rule classes
+    that inherit from `_Rules()`.
+    """
+    _registry = {}
+
+    def __new__(mcs, name, bases, attrs):
+        cls = type.__new__(mcs, name, bases, attrs)
+        name = cls.TYPENAME or cls.__name__.lower()
+        if not name.startswith('_'):
+            cls.TYPENAME = name
+            mcs._registry[name] = cls
+        return cls
+
+    @classmethod
+    def get(mcs):
+        return mcs._registry
+
+
 class _ListRules(list):
     """
     Subclass of `list()` that makes sure each element of it is an instance of
@@ -61,8 +82,6 @@ class Converter(object):
     """
     A base class that can apply a (ordered) list of rules to a dict.
     """
-    _map_rules_cls = {}
-
     def __init__(self, rule_cls, rules=[]):
         if not issubclass(rule_cls, _Rule):
             err = '{obj} is not a subclass of _Rule'
@@ -72,7 +91,7 @@ class Converter(object):
 
     @property
     def type(self):
-        return self._rule_cls.__name__
+        return self._rule_cls.TYPENAME
 
     @property
     def rules(self):
@@ -84,14 +103,14 @@ class Converter(object):
         Create a `Converter()` object from dict 'config'. The dict must look
         like the following:
             {
-                "rule_cls": <rule-type-name>,   # lower-case
+                "type": <rule-type-name>,       # lower-case
                 "rules": [
                     {"fieldname": <name>, ...}, # + rule-type dependent items
                     {"fieldname": <name>, ...},
                 ]
             }
         """
-        rule_cls = cls._map_rules_cls[config['rule_cls']]
+        rule_cls = _RegisteredRules.get()[config['type']]
         rules = []
         for params in config['rules']:
             rules.append(rule_cls(**params))
@@ -99,34 +118,25 @@ class Converter(object):
 
     def apply(self, data):
         """
-        Apply each rule sequentially
+        Apply each rule sequentially to `data` (in-place update)
         """
         for rule in self.rules:
             rule.apply(data)
 
 
-def register(cls):
-    """
-    A decorator that populates the `Converter` class so as to link rule classes
-    to rule type names.
-    """
-    cls.typename = cls.__name__.lower()
-    Converter._map_rules_cls[cls.typename] = cls
-    return cls
-
-
-class _Rule(object):
+class _Rule(metaclass=_RegisteredRules):
     """
     A rule is applied to a dict and may update that dict (in-place update).
     Only one field from the input dict can be processed within a rule.
     """
-    typename = None
+    TYPENAME = None
 
     def __init__(self, fieldname, **kwargs):
         self.fieldname = fieldname
-        self.configure(**kwargs)
+        self.config = kwargs
+        self.configure()
 
-    def configure(self, *args, **kwargs):
+    def configure(self):
         """
         Configure the rule to be applied (e.g. compile a regexp) so that
         everything is ready at runtime to apply it.
@@ -145,22 +155,22 @@ class _Rule(object):
 class _RegexpRule(_Rule):
     """
     Define a regular expression that shall be applied through several kinds of
-    operations to a string read from the input `data` dict.
+    operations to a string.
     If the regexp pattern has named capturing parenthesis it is used to update
-    the input dict. If a group from the match object and a key from the input
-    dict have the same name, the group overrides the existing dict value.
+    the `data` dict passed to `apply()`. If a group from the match object and
+    a key from `data` have the same name, the group overrides the existing
+    dict value.
+
+    Valid `__init__` kwargs are: 'pattern', 'flags'
     """
     def _compile(self, pattern, flags=0):
-        self.regexp = re.compile(pattern, flags=flags)
+        self.config['regexp'] = re.compile(pattern, flags=flags)
 
-    def _set_params(self, **kwargs):
-        pass
-
-    def configure(self, pattern='', flags=0, **kwargs):
+    def configure(self):
+        pattern, flags = self.config['pattern'], self.config.get('flags')
         self._compile(pattern, flags=flags)
-        self._set_params(**kwargs)
 
-    def _run_regexp(self, *args, **kwargs):
+    def _run_regexp(self, string):
         pass
 
     def apply(self, data):
@@ -178,70 +188,75 @@ class _MatchSearchRule(_RegexpRule):
     """
     Match and search operations basically share a common logic: when the
     operation produces a match what matters is the resulting groupdict.
-    """
-    _regexp_op = None
 
-    def _set_params(self, pos=None, endpos=None):
-        self.pos = pos
-        self.endpos = endpos
-        self._pos_args = tuple(f for f in (pos, endpos) if f is not None)
+    Valid `__init__` kwargs are: 'pattern', 'flags', 'pos', 'endpos'
+    """
+    def configure(self):
+        super(_MatchSearchRule, self).configure()
+        pos, endpos = self.config['pos'], self.config['endpos']
+        pos_args = tuple(f for f in (pos, endpos) if f is not None)
+        self.config['pos_args'] = pos_args
 
     def _run_regexp(self, string):
-        args = (string,) + self._pos_args
-        match = getattr(self.regexp, self._regexp_op)(*args)
+        args = (string,) + self.config['pos_args']
+        match = getattr(self.config['regexp'], self.TYPENAME)(*args)
         if match is not None:
             return match.groupdict()
         else:
             return {}
 
 
-@register
+# Search.TYPENAME
+# s = Search(*args, **kwargs)
+# s._configure(*args, **kwargs)
+# s.fieldname
+# s.config
+# s.apply(<dict>)
 class Search(_MatchSearchRule):
     """
     Scan through a string looking for the first location where the regular
     expression pattern produces a match.
+
+    Valid `__init__` kwargs are: see `_MatchSearchRule`
     """
-    _regexp_op = 'search'
 
 
-@register
 class Match(_MatchSearchRule):
     """
     Check if zero or more characters at the beginning of a string match the
     regular expression pattern.
+
+    Valid `__init__` kwargs are: see `_MatchSearchRule`
     """
-    _regexp_op = 'match'
 
 
-@register
 class Sub(_RegexpRule):
     """
     Build a string obtained by replacing the leftmost non-overlapping
     occurrences of regexp pattern in fieldname from `data` by a replacement
     string.
-    """
-    def _set_params(self, repl='', count=0):
-        self.repl = repl
-        self.count = count
 
+    Valid `__init__` kwargs are: 'pattern', 'flags', 'repl', 'count'
+    """
     def _run_regexp(self, string):
-        res = self.regexp.sub(self.repl, string, count=self.count)
+        regexp, repl = self.config['regexp'], self.config['repl']
+        res = regexp.sub(repl, string, count=self.config['count'])
         return {self.fieldname: res}
 
 
-@register
 class Set(_Rule):
     """
     Set or update a field in the `data` dict.
+
+    Valid `__init__` kwargs is: 'value'
     """
-    def configure(self, value=''):
-        self.value = value
+    def configure(self):
+        pass
 
     def apply(self, data):
-        data[self.fieldname] = self.value
+        data[self.fieldname] = self.config['value']
 
 
-@register
 class Unset(_Rule):
     """
     Remove a field from the `data` dict.
@@ -256,18 +271,15 @@ class Unset(_Rule):
             pass
 
 
-@register
 class Lookup(_Rule):
     """
-    Implements a lookup table where the input value from `data` must match a
-    regexp to be replaced by a new value from the table.
+    Implements a dead-simple lookup table which perform case sensitive
+    (default) or insensitive (icase=True) lookups.
+
+    Valid `__init__` kwargs are: 'icase', 'table' (a dict)
     """
-    def configure(self, table={}):
-        """
-        'table' is a dict where keys can be a dict or a string that represents
-        the regexp pattern (see LookupTable).
-        """
-        self.table = LookupTable(table)
+    def configure(self):
+        self.config['table'] = LookupTable(self.config['table'])
 
     def apply(self, data):
         """
@@ -276,7 +288,7 @@ class Lookup(_Rule):
         unpredictable order.
         """
         field = data[self.fieldname]
-        for regexp, value in self.table.items():
+        for regexp, value in self.config['table'].items():
             if regexp.search(field) is not None:
                 data[self.fieldname] = value
                 break
