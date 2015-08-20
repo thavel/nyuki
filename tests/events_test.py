c0808574aaf410683f9c405e98be74a3ad4f4f2c
thibaut <thibaut.havel@gmail.com>
2015-08-20 15:17:18 +0200

    Add unit tests on events.

--- tests/events_test.py
+++ tests/events_test.py
@@ -4,35 +4,48 @@ from mock import Mock
 from nyuki.events import EventManager, Event, on_event
 
 
-class Loop(Mock):
-    STACK = list()
+class TestOnEvent(TestCase):
 
-    def async(self, cb, *args):
-        self.STACK.append(cb)
-
+    def test_001_call(self):
+        @on_event(Event.Connected, Event.Disconnected)
+        def test():
+            pass
+        self.assertIsInstance(test.on_event, set)
 
 
 class TestEventManager(TestCase):
 
     def setUp(self):
-        loop = Mock()
-        self.manager = EventManager(loop)
+        self.loop = Mock()
+        self.manager = EventManager(self.loop)
 
     def test_001_init(self):
-        # Ensure callback tree has been properly setup.
+        # Ensure callback tree has been properly setup
         self.assertCountEqual(self.manager._callbacks.keys(), list(Event))
 
     def test_002a_register(self):
-        pass
+        # For all kind of event, we're ensure we can add a callback
+        for event in Event:
+            callback = (lambda x: x)
+            self.manager.register(event, callback)
+            self.assertIn(callback, self.manager._callbacks[event])
 
     def test_002b_register_error(self):
-        pass
+        # Here's what happens when the specified event does not exists
+        event = Mock()
+        callback = (lambda x: x)
+        self.assertRaises(ValueError, self.manager.register, event, callback)
 
     def test_003a_trigger(self):
-        pass
+        # Ensure callbacks are properly scheduled when an event is triggered
+        callbacks = list()
+        self.loop.async = (lambda c, *args: callbacks.append(c))
+        cb = (lambda x: x)
+        self.manager.register(Event.Connected, cb)
+        self.manager.trigger(Event.Connected)
+        self.assertIn(cb, callbacks)
 
     def test_003b_trigger_error(self):
-        pass
-
-    def tearDown(self):
-        pass
+        # Ensure we can not trigger an event that does not exists
+        event = Mock()
+        self.assertRaises(ValueError, self.manager.trigger, event)
