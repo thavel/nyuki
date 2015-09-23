e5ded18ca4f1d7a37b3a5fb19abdd4c3c921c662
Guillaume Desvé <guillaume.desve@surycat.com>
2015-09-23 18:08:03 +0200

    Add MANIFEST and update setup.py for pypi

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 
-with open('VERSION.txt') as fp:
-    version = fp.read().strip()
+with open('VERSION.txt', 'r') as v:
+    version = v.read()
 
 # Requirements
 install_reqs = parse_requirements('requirements.txt', session='dummy')
@@ -13,10 +13,20 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='nyuki',
-    version=version,
     description='Nyuki library',
+    url='http://www.surycat.com',
     author='Optiflows R&D',
     author_email='rand@surycat.com',
+    version=version,
     install_requires=reqs,
     packages=find_packages(exclude=['tests']),
+    license='Apache 2.0',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only',
+    ),
 )
