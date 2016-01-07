dd401077da7974394f8a6e1c6ae2c0fd765accc7
Ketouem <cyril.thomas@surycat.com>
2016-01-07 15:33:03 +0100

    Release v0.8.2

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3 :: Only',
     ],
 )
