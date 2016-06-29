c9937beeca0cb2ba35c0d29b1962c445a61eed35
Guillaume Desvé <guillaume.desve@surycat.com>
2016-06-29 11:51:19 +0200

    proper reconnection + persistence

--- nyuki/bus/mqtt.py
+++ nyuki/bus/mqtt.py
@@ -1,5 +1,6 @@
 import asyncio
-from hbmqtt.client import MQTTClient, ClientException, ConnectException
+from datetime import datetime
+from hbmqtt.client import MQTTClient, ConnectException
 from hbmqtt.errors import NoDataException
 from hbmqtt.mqtt.constants import QOS_2
 import json
@@ -7,7 +8,7 @@ import logging
 import re
 from uuid import uuid4
 
-# from nyuki.bus.persistence import BusPersistence, EventStatus, PersistenceError
+from nyuki.bus.persistence import BusPersistence, EventStatus, PersistenceError
 from nyuki.services import Service
 
 
@@ -22,14 +23,6 @@ class RequestAPI(object):
 
     async def json(self):
         return self._body
-
-
-class ReMQTTClient(MQTTClient):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # We handle that ourselves
-        self.config['auto_reconnect'] = False
 
 
 class MqttBus(Service):
@@ -87,56 +80,90 @@ class MqttBus(Service):
         self._loop = loop or asyncio.get_event_loop()
         self._host = None
         self._self_topic = None
-        self._request_topic = None
-        self.client = ReMQTTClient(loop=self._loop)
+        # self._request_topic = None
+        self.client = MQTTClient(
+            config={'auto_reconnect': False},
+            loop=self._loop
+        )
         self._pending = {}
         self.name = None
         self._default_callback = None
         self._subscriptions = {}
+        self._disconnection_datetime = None
 
         # Coroutines
-        self._frun = None
+        self.connect_future = None
+        self.listen_future = None
 
-    def configure(self, host, name, port=1883, certificate=None):
+    def configure(self, host, name, port=1883, certificate=None, persistence={}):
         self._host = '{}:{}'.format(host, port)
         self.name = name
         self._self_topic = self._publish_topic(self.name)
-        self._request_topic = '{}/{}/+/+'.format(self.BASE_REQ, self.name)
+        # self._request_topic = '{}/{}/+/+'.format(self.BASE_REQ, self.name)
         self._certificate = certificate
 
-    async def start(self):
-        self._frun = asyncio.ensure_future(self._run(), loop=self._loop)
+        # Persistence storage
+        self._persistence = BusPersistence(name=name, **persistence)
+        log.info('Bus persistence set to %s', self._persistence.backend)
 
-        # try:
-        #     await self._persistence.init()
-        # except PersistenceError:
-        #     log.error(
-        #         'Could not init persistence storage with %s',
-        #         self._persistence.backend
-        #     )
+    async def start(self):
+        def cancelled(future):
+            try:
+                future.result()
+            except asyncio.CancelledError:
+                log.debug('future cancelled: %s', future)
+        self.connect_future = asyncio.ensure_future(self._run())
+        self.connect_future.add_done_callback(cancelled)
+
+        try:
+            await self._persistence.init()
+        except PersistenceError:
+            log.error(
+                'Could not init persistence storage with %s',
+                self._persistence.backend
+            )
 
     async def stop(self):
         # Clean tasks
         for task in self.client.client_tasks:
             log.debug('cancelling mqtt client tasks')
             task.cancel()
-        if self._frun:
+        if self.connect_future:
             log.debug('cancelling _run coroutine')
-            self._frun.cancel()
+            self.connect_future.cancel()
+        if self.listen_future:
+            log.debug('cancelling _listen coroutine')
+            self.listen_future.cancel()
         if self.client._connected_state.is_set():
             log.debug('disconnecting mqtt client')
             await self.client.disconnect()
+        await self._persistence.close()
         log.info('MQTT service stopped')
 
     def _publish_topic(self, nyuki):
         return '{}/{}'.format(self.BASE_PUB, nyuki)
 
-    def _resp_topic_from_req(self, req_topic):
-        return re.sub(
-            r'%s/' % self.BASE_REQ,
-            r'%s/' % self.BASE_RESP,
-            req_topic
-        )
+    async def replay(self, since=None, status=None, wait=0):
+        """
+        Replay events since the given datetime (or all if None)
+        """
+        log.info('Replaying events')
+        if since:
+            log.info('    since %s', since)
+        if status:
+            log.info('    with status %s', status)
+
+        if wait:
+            log.info('Waiting %d seconds', wait)
+            await asyncio.sleep(wait)
+
+        events = await self._persistence.retrieve(since, status)
+        for event in events:
+            await self.publish(json.loads(
+                event['message']),
+                event['topic'],
+                event['id']
+            )
 
     def set_default_callback(self, callback):
         """
@@ -146,17 +173,19 @@ class MqttBus(Service):
             raise ValueError('event callback must be a coroutine')
         self._default_callback = callback
 
-    async def subscribe(self, topic, callback=None):
+    async def subscribe(self, topic, callback):
         """
-        Subscribe, with callback if given
-        (wildcards won't trigger callbacks)
+        Subscribe to a topic and setup the callback
         """
-        if callback:
-            if not asyncio.iscoroutinefunction(callback):
-                raise ValueError('event callback must be a coroutine')
-            self._subscriptions[topic] = callback
+        if not asyncio.iscoroutinefunction(callback):
+            raise ValueError('event callback must be a coroutine')
         log.debug('MQTT subscription to %s', topic)
         await self.client.subscribe([(topic, QOS_2)])
+        regex = r'^{}$'.format(
+            topic.replace('+', '[^\/]+').replace('#', '.+')
+        )
+        self._subscriptions[re.compile(regex)] = callback
+        log.info('Callback set on regex: %s', regex)
 
     async def unsubscribe(self, topic):
         """
@@ -167,53 +196,36 @@ class MqttBus(Service):
         if topic in self._subscriptions:
             del self._subscriptions[topic]
 
-    async def publish(self, data, topic=None):
+    async def publish(self, data, topic=None, previous_uid=None):
         """
         Publish in given topic or default one
         """
+        uid = previous_uid or str(uuid4())
         topic = topic or self._self_topic
         log.info('Publishing into %s', topic)
         data = json.dumps(data)
         log.debug('dump: %s', data)
-        # Implies QOS_0
-        await self.client.publish(topic, data.encode())
 
-    async def request(self, nyuki, capability, data):
-        req_topic = '{}/{}/{}/{}'.format(
-            self.BASE_REQ,
-            nyuki,
-            capability,
-            str(uuid4())[:8]
-        )
-        resp_topic = self._resp_topic_from_req(req_topic)
-        future = asyncio.Future()
-        self._pending[resp_topic] = future
+        if not self.client._connected_state.is_set():
+            status = EventStatus.PENDING
+        else:
+            # Implies QOS_0
+            await self.client.publish(topic, data.encode())
+            status = EventStatus.SENT
 
-        async def cb(data):
-            future.set_result(data)
-
-        # Subscribe to the request response
-        await self._sub(resp_topic, cb)
-        # Publish the request
-        await self.publish(data, req_topic)
-
-        # Wait for the response
-        await future
-
-        # Retrieve the response and unsubscribe
-        response = future.result()
-        del self._pending[resp_topic]
-        await self._unsub(resp_topic)
-
-        log.info('Response received: %s', response)
-        return response
-
-    async def reply(self, topic, data):
-        resp_topic = self._resp_topic_from_req(topic)
-        if not resp_topic.startswith(self.BASE_RESP + '/'):
-            log.error('Reply failure: %s', resp_topic)
-            return
-        await self.publish(data, resp_topic)
+        if previous_uid:
+            await self._persistence.update(previous_uid, status)
+        elif self._persistence:
+            await self._persistence.store({
+                'id': uid,
+                'status': status.value,
+                'topic': topic,
+                'message': data,
+            })
+            if self._persistence.memory_buffer.is_full:
+                asyncio.ensure_future(self._nyuki.on_buffer_full(
+                    self._persistence.memory_buffer.free_slot
+                ))
 
     async def _run(self):
         """
@@ -221,37 +233,42 @@ class MqttBus(Service):
         """
         delay = 1
         while True:
+            log.info('Trying MQTT connection to %s', self._host)
             try:
-                log.info('Trying MQTT connection to %s', self._host)
-                try:
-                    await self.client.connect(
-                        self._host,
-                        cafile=self._certificate
-                    )
-                except (ConnectException, NoDataException) as e:
-                    log.exception(e)
-                    current_delay = (delay * 2) + 1
-                    log.info('Waiting %d seconds to reconnect', current_delay)
-                    await asyncio.sleep(current_delay)
-                    delay = current_delay
-                    continue
+                await self.client.connect(
+                    self._host,
+                    cafile=self._certificate
+                )
+            except (ConnectException, NoDataException) as e:
+                log.exception(e)
+                # TODO: useless mechanism ?
+                current_delay = (delay * 2) + 1
+                log.info('Waiting %d seconds to reconnect', current_delay)
+                await asyncio.sleep(current_delay)
+                delay = current_delay
+                continue
 
-                # Reset reconnection delay
-                delay = 1
-                log.info('Connection made with MQTT')
-                # Subscribe to nyuki publications
-                # await self._sub()
-                # Subscribe to own requests topic
-                # await self._sub(self._request_topic, self._handle_request)
+            # Reset reconnection delay
+            delay = 1
+            log.info('Connection made with MQTT')
 
-                try:
-                    await self._listen()
-                except ClientException as e:
-                    log.exception(e)
+            # Replaying events
+            if self._persistence and self._disconnection_datetime:
+                asyncio.ensure_future(self.replay(
+                    self._disconnection_datetime,
+                    EventStatus.not_sent(),
+                    3
+                ))
 
-            except asyncio.CancelledError:
-                log.debug('Connection loop cancelled')
-                break
+            self._disconnection_datetime = None
+            # Start listening
+            self.listen_future = asyncio.ensure_future(self._listen())
+            # Blocks until mqtt is disconnected
+            await self.client._handler.wait_disconnect()
+            self._disconnection_datetime = datetime.utcnow()
+            # Clean listen_future
+            self.listen_future.cancel()
+            self.listen_future = None
 
     async def _listen(self):
         """
@@ -265,55 +282,8 @@ class MqttBus(Service):
             if topic == self._publish_topic(self.name):
                 continue
 
-            cb = self._subscriptions.get(topic, self._default_callback)
-            if not cb:
-                log.warning('no callback for event')
-                continue
-
-            asyncio.ensure_future(
-                self._handle_event(message, cb),
-                loop=self._loop
-            )
-            # Check request
-            # elif re.match(r'^%s/%s/\w+/\w{8}$' % (self.BASE_REQ, self.name), topic):
-            #     asyncio.ensure_future(self._handle_request(message), loop=self._loop)
-
-    async def _handle_event(self, message, callback):
-        """
-        Handle an event from mqtt
-        """
-        data = json.loads(message.data.decode())
-        log.debug("Event from topic '%s': %s", message.topic, data)
-        await callback(message.topic, data)
-
-    async def _handle_request(self, message):
-        """
-        Dispatch either a request or a response
-        """
-        # Check topic validity
-        m = re.match(
-            r'^(?P<mtype>\w+)/'
-            r'(?P<nyuki_name>\w+)/'
-            r'(?P<capability>\w+)/'
-            r'(?P<uuid>\w{8})$',
-            message.topic
-        )
-        if not m:
-            log.debug('Topic did not match: %s', message.topic)
-            return
-
-        data = json.loads(message.data.decode())
-        log.info("Message from topic '%s'", message.topic)
-        log.debug('dump: %s', data)
-
-        if m.group('mtype') == self.BASE_RESP and message.topic in self._pending:
-            # Is a pending response, set future result
-            self._pending[message.topic].set_result(data)
-        elif m.group('mtype') == self.BASE_REQ:
-            # Is a request
-            resp = await self._nyuki.api.call(
-                m.group('capability'), RequestAPI(data)
-            )
-            await self.reply(message.topic, resp)
-        else:
-            log.error('Message type does not match: %s', m.group('mtype'))
+            for regex, callback in self._subscriptions.items():
+                if regex.match(topic):
+                    data = json.loads(message.data.decode())
+                    log.debug("Event from topic '%s': %s", message.topic, data)
+                    asyncio.ensure_future(callback(topic, data))
