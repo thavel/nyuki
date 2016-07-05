43815483664eba5d92b15dc5af2528c48dd1a912
Guillaume Desvé <guillaume.desve@surycat.com>
2016-07-05 14:28:50 +0200

    Add custom schema for the base join task

--- nyuki/workflow/tasks/__init__.py
+++ nyuki/workflow/tasks/__init__.py
@@ -1,7 +1,31 @@
+from tukio.task.join import JoinTask
+
 from .factory import FactoryTask
 from .report import ReportTask
 from .sleep import SleepTask
 
+
+# Add a schema to the join task
+JoinTask.SCHEMA = {
+    'type': 'object',
+    'required': ['wait_for'],
+    'properties': {
+        'wait_for': {
+            'type': 'array',
+            'minItems': 2,
+            'maxItems': 64,
+            'uniqueItems': True,
+            'items': {
+                'type': 'string',
+                'maxLength': 1024
+            }
+        },
+        'timeout': {
+            'type': 'integer',
+            'minimum': 1
+        }
+    }
+}
 
 # Generic schema to reference a task ID
 TASKID_SCHEMA = {
