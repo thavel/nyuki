e01d923a73d94937915671451f2116981895e120
Ludovic Havel <ludovic.havel@surycat.com>
2016-08-02 11:27:12 +0200

    refactored 'arithmetics.py' module

--- nyuki/workflow/tasks/utils/arithmetics.py
+++ nyuki/workflow/tasks/utils/arithmetics.py
@@ -1,15 +1,21 @@
 import ast
 
 
-TYPES = [
+EXPRESSIONS = [
+    # Types of values
     ast.Dict,
     ast.List,
     ast.NameConstant,
     ast.Num,
     ast.Set,
     ast.Str,
-    ast.Tuple
+    ast.Tuple,
+    # Types of operations
+    ast.Compare,
+    ast.BoolOp,
+    ast.UnaryOp
 ]
+
 OPERATORS = [
     ast.And,
     ast.Eq, ast.NotEq,
@@ -23,57 +29,19 @@ OPERATORS = [
     ast.UAdd,
     ast.USub
 ]
-EXPRESSIONS = [ast.Compare, ast.BoolOp, ast.UnaryOp]
-ATTR_OPS = ['op', 'ops']
-ATTR_VALUES = ['values', 'operand', 'comparators', 'left']
-ATTR_COLLECTIONS = ['elts', 'keys', 'values']
+
+CONTEXTS = [ast.Load]
+
+AUTHORIZED_TYPES = EXPRESSIONS + OPERATORS + CONTEXTS
 
 
-def arithmetic_eval(expr):
+def safe_eval(expr):
     """
-    Safely eval an arithmetic expression (for a condition block).
+    Ensures an expression only defines authorized operations (no call to
+    functions, no variable assignement...) and evaluates it.
     """
-    node = ast.parse(expr, mode='eval').body
-    _is_arithmetic(node)
+    tree = ast.parse(expr, mode='eval').body
+    for node in ast.walk(tree):
+        if not type(node) in AUTHORIZED_TYPES:
+            raise TypeError("forbidden type {} found in {}".format(node, expr))
     return bool(eval(expr))
-
-
-def _is_arithmetic(node):
-    """
-    Recursive method that raises either a `TypeError` or a `SyntaxError` if
-    the given expression is not a valid and safe arithmetic expression.
-    """
-    ntype = type(node)
-
-    # Check types
-    if ntype in TYPES:
-        for coll in ATTR_COLLECTIONS:
-            for item in getattr(node, coll, []):
-                _is_arithmetic(item)
-        return
-
-    # Check expressions
-    if ntype not in EXPRESSIONS:
-        raise TypeError("Not allowed expression: {}".format(ntype))
-
-    # Check operators
-    for op_name in ATTR_OPS:
-        ops = getattr(node, op_name, [])
-
-        if isinstance(ops, list):
-            ops_type = set([type(op) for op in ops])
-            ops_invalid = ops_type.difference(OPERATORS)
-        else:
-            ops_type = type(ops)
-            ops_invalid = ops_type if ops_type not in OPERATORS else {}
-        if ops_invalid:
-            raise TypeError("Not allowed operators: {}".format(ops_invalid))
-
-    # Check values
-    for val_name in ATTR_VALUES:
-        vals = getattr(node, val_name, [])
-        if isinstance(vals, list):
-            for val in vals:
-                _is_arithmetic(val)
-        else:
-            _is_arithmetic(vals)
