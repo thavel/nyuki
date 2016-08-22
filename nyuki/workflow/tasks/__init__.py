d343746bd2cefa2cfc29b66bfc1e80093b8ba1c4
Guillaume Desvé <guillaume.desve@surycat.com>
2016-08-22 16:19:04 +0200

    use taskid_schema in jointask schema

--- nyuki/workflow/tasks/__init__.py
+++ nyuki/workflow/tasks/__init__.py
@@ -8,6 +8,15 @@ from .sleep import SleepTask
 from .task_selector import TaskSelector
 
 
+# Generic schema to reference a task ID
+TASKID_SCHEMA = {
+    'type': 'string',
+    'description': 'task_id',
+    'minLength': 1,
+    'maxLength': 128
+}
+
+
 # Add a schema to the join task
 JoinTask.SCHEMA = {
     'type': 'object',
@@ -20,22 +29,11 @@ JoinTask.SCHEMA = {
                     'minItems': 2,
                     'maxItems': 64,
                     'uniqueItems': True,
-                    'items': {
-                        'type': 'string',
-                        'minLength': 1,
-                        'maxLength': 1024
-                    }
+                    'items': TASKID_SCHEMA
                 },
                 {'type': 'integer', 'minimum': 2}
             ]
         },
         'timeout': {'type': 'integer', 'minimum': 1}
     }
-}
-
-# Generic schema to reference a task ID
-TASKID_SCHEMA = {
-    'type': 'string',
-    'description': 'task_id',
-    'maxLength': 128
 }
