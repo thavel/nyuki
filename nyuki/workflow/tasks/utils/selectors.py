1994550484aa912220f357c395df719aec96a948
Guillaume Desvé <guillaume.desve@surycat.com>
2016-08-22 15:42:22 +0200

    Add base selector schema

---
+++ nyuki/workflow/tasks/utils/selectors.py
@@ -0,0 +1,66 @@
+SELECTOR_SCHEMA = {
+    'type': 'object',
+    'required': ['selectors'],
+    'properties': {
+        'selectors': {
+            'type': 'array',
+            'items': {
+                'oneOf': [
+                    {'$ref': '#/definitions/condition-block'},
+                    {'$ref': '#/definitions/selector'}
+                ]
+            }
+        }
+    },
+    'definitions': {
+        'values': {
+            'type': 'array',
+            'items': {
+                'type': 'string',
+                'minLength': 1,
+                'uniqueItems': True
+            }
+        },
+        'selector': {
+            'type': 'object',
+            'required': ['type', 'values'],
+            'properties': {
+                'type': {'type': 'string', 'enum': ['selector']},
+                'values': {'$ref': '#/definitions/values'}
+            }
+        },
+        'condition-block': {
+            'type': 'object',
+            'required': ['type', 'conditions'],
+            'properties': {
+                'type': {'type': 'string', 'enum': ['condition-block']},
+                'conditions': {
+                    'type': 'array',
+                    'items': {
+                        'oneOf': [
+                            {'$ref': '#/definitions/condition-if'},
+                            {'$ref': '#/definitions/condition-else'}
+                        ]
+                    }
+                }
+            }
+        },
+        'condition-if': {
+            'type': 'object',
+            'required': ['type', 'condition', 'values'],
+            'properties': {
+                'type': {'type': 'string', 'enum': ['if', 'elif']},
+                'condition': {'type': 'string', 'minLength': 1},
+                'values': {'$ref': '#/definitions/values'}
+            }
+        },
+        'condition-else': {
+            'type': 'object',
+            'required': ['type', 'values'],
+            'properties': {
+                'type': {'type': 'string', 'enum': ['else']},
+                'values': {'$ref': '#/definitions/values'}
+            }
+        }
+    }
+}
