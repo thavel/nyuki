9b8cd39c074b79cd56f500d067944935f7a8c74a
Laurent Erignoux <lerignoux@gmail.com>
2017-01-23 17:51:35 +0800

    Adding error details in api response for debugging

--- nyuki/utils/transform.py
+++ nyuki/utils/transform.py
@@ -192,7 +192,7 @@ class _Rule(metaclass=_RegisteredRule):
             try:
                 func(self, tracker)
             except RegexpRuleError as exc:
-                return {'type': self.TYPENAME, 'changes': tracker.changes, 'error': 'regexp_rule_error'}
+                return {'type': self.TYPENAME, 'changes': tracker.changes, 'error': 'regexp_rule_error', 'error_details': str(exc)}
             # We want to keep the object reference
             data.clear()
             data.update(tracker)
